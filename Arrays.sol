fc17d7de872fe95a803f3d372a3701bd53f17577
cl-rs <81256027+cl-rs@users.noreply.github.com>
2022-03-14 23:19:27 +0530

    Update Arrays.sol

--- Arrays.sol
+++ Arrays.sol
@@ -1,1 +1,24 @@
 
+pragma solidity ^0.4.17
+
+contract Test
+{
+  uint[] pubic myArray;
+  
+  function Test() public
+  {
+      myArray.push(1);
+      myArray.push(10);
+      myArray.push(30);
+      
+   }
+	 
+	function getArrayLength() public view returns(uint)
+	{
+		return myArray.length;
+		
+	}
+	
+	
+	 		
+   
