883975b438d36932edc889d0e679b7e5c8142b08
Bhushan <tucater10896601@protonmail.com>
2022-04-28 17:29:43 +0530

    added wavePoratal.sol kinda like the hello world of solidity

---
+++ my-wave-portal/contracts/WavePortal.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier :UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal 
+{
+    constructor()
+    {
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+}
+
+
+
