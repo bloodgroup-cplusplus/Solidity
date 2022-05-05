719a38f7c85912c6f7401ed538f375dcdb09256c
Bhushan <tucater10896601@protonmail.com>
2022-05-05 12:12:15 +0530

    override componentDidMount in AvatarView. Created a new WEBGLREnderer, configure it to fit our window and append it to main view

--- BuildAvatar/scripts/AvatarView.js
+++ BuildAvatar/scripts/AvatarView.js
@@ -4,6 +4,7 @@
 //
 
 import React from 'react'
+import * as THREE from "three" const navigationBarHeight = 100
 export class AvatarView extends ReactComponent
 {
     mainViewRef = React createRef()
@@ -19,5 +20,30 @@ export class AvatarView extends ReactComponent
         />
 
     }}
+
+// Import Three.js (second line ) and override DidMount in AvatarView .
+// Then we can create a new WebGLRenderer, Configure it to fit our window and 
+// some other lighting settings
+// After that, append it onto our main view 
+
+
+
+async componentDidMount()
+{
+    const mainView = this mainViewRef.current 
+    this renderer = new THREE WebGLRenderer({antialias:true})
+    this renderer.setSize(window.innerWidth, window.innerHeight - navigationBarHeight)
+    this.renderer.outputEncoding = THREE.SRGBEncoding
+    this.renderer.toneMapping = THREE.ACESFilmicToneMapping
+    main View.appendChild (this.renderer.domElement)
+
+// adding the camera and scene will allow us to view our model 
+    // we will be loading a 3d file to supply both the scenes background
+    // texture and the lighting 
+    // we will also add the orbit controls from the three.js exmaple to 
+    // better inspect our ready player me.
+
+}
+
 
 
