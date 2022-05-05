feca48d219d39a9ca326f1816126a666c6df15ec
Bhushan <tucater10896601@protonmail.com>
2022-05-05 12:17:36 +0530

    loader function added

--- BuildAvatar/scripts/AvatarView.js
+++ BuildAvatar/scripts/AvatarView.js
@@ -43,7 +43,35 @@ async componentDidMount()
     // we will also add the orbit controls from the three.js exmaple to 
     // better inspect our ready player me.
 
+  const mainView = this.mainViewRef.current  this.renderer = new THREE.WebGLRenderer({ antialias: true })
+  this.renderer.setSize(window.innerWidth, window.innerHeight - navigationBarHeight)
+  this.renderer.outputEncoding = THREE.sRGBEncoding
+  this.renderer.toneMapping = THREE.ACESFilmicToneMapping  mainView.appendChild(this.renderer.domElement)  this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / (window.innerHeight - navigationBarHeight), 0.1, 1000)
+
+  this.camera.position.set(0, 0, 3)
+  this.controls = new OrbitControls(this.camera, this.renderer.domElement)// We will use the lighting from the background instead of creating our own
+  const background = await this.loadBackground(backgroundUrl, this.renderer)
+  this.scene = new THREE.Scene()
+  this.scene.environment = background
+  this.scene.background = background
+    
 }
+// add a loader function to help us get the file added to our environemt 
+
+loadBackground (url.renderer)
+{
+    return new Promise((resolve)) =>{
+        const loader = new RGBELoader()
+        const generator = new THREE.PMREMGenerator(renderer)
+        loader load(url, texture) =>{
+            const envMap = generator.fromEquirectangular(texture) texture
+            generator.dispose()
+            texture.dispose()
+            resolve(envMap)
+        })
+    })
+}
+
 
 
 
