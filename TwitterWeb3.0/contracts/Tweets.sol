021f9495e99fedac19ae9e906cb25dd60b555959
Bhushan <tucater10896601@protonmail.com>
2022-05-10 07:45:19 +0530

    Full twitter smart contract added

--- TwitterWeb3.0/contracts/Tweets.sol
+++ TwitterWeb3.0/contracts/Tweets.sol
@@ -31,12 +31,12 @@ contract tweets
     // create an event when a new tweet is created on the smart contract 
 
     event tweetCreated
-    {
+    (
         address tweeter,
         uint256 id, 
         string tweetTxt,
-        string tweetIng,
-    }
+        string tweetIng
+    );
 
     // store all of these tweets in some sort of array 
 
@@ -46,3 +46,53 @@ contract tweets
 
 
     // function to add a tweet 
+
+    function addTweet
+    (
+        string memory tweetTxt,
+        string memory tweetImg
+    )
+
+    public payable
+    {
+        require(msg.value == (1 ether), "Please submit 1 matic");
+        tweet storage newTweet = Tweets[counter];
+        newTweet.tweetTxt = tweetTxt;
+        newTweet.tweetIng = tweetIng;
+        newTweet.tweeter = msg.sender;
+        newTweet.id = counter;
+        emit tweetCreated(
+            msg.sender,
+            counter,
+            tweetTxt,
+            tweetImg
+        );
+
+        counter++;
+
+        payable(owner).transfer(msg.value);
+        // we are requiring the message value of one ether make sure that it is send 
+        // to the owner of the smart contract 
+        // payable to the owner 
+        // transfer the message value 
+    }
+
+    // what this function does is that as long as you pass parameter of id 
+    // public function that returns string 
+    // and these two strings will be tweet image source 
+    // adress of log of all the tweets 
+    // what the address was who tweeted it 
+    // 
+
+    function getTweet(uint256 id) public view returns (string memory, string memory, address)
+    {
+        require(id < counter, "No such tweet");
+        tweet storage t = Tweets[id];
+        return (t.tweetTxt, t.tweetImg, t.tweeter);
+    }
+}
+
+
+
+
+    }
